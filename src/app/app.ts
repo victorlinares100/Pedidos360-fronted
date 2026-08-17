327e27521e8e8b204fe276a54242a1cb84faaecd
victorlinares100 <linares28072004@gmail.com>
2026-08-17 10:25:07 -0400

    iniciando angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pedidos360-fronted');
+  protected readonly title = signal('pedidos360-frontend');
 }
